0ba25dfd5fd5b732f3371e4bf018cbb690fb3df0
Leia <e.leiachang@gmail.com>
2022-05-13 14:49:06 -0400

    5-motor control

--- expedition-earth/interactive-plant/multiplexed-servo/multiplexed-servo.ino
+++ expedition-earth/interactive-plant/multiplexed-servo/multiplexed-servo.ino
@@ -1,52 +1,79 @@
-/***************************************************
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 8 servos, one after the other on the
-  first 8 pins of the PCA9685
+#include "secrets.h"
 
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include "Adafruit_VL53L0X.h"
 
-  These drivers use I2C to communicate, 2 pins are required to
-  interface.
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
 
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
+WiFiUDP Udp;
+const unsigned int localPort = 8888;        // local port to listen for UDP packets (here's where we send the packets)
+OSCErrorCode error;
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+bool useWifi = false;
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
 // Depending on your servo make, the pulse width min and max may vary, you
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  100 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  500 // This is the 'maximum' pulse length count (out of 4096)
 #define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
 #define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define SERVO_SPEED 6 // Speed that the servo moves
 
 // our servo # counter
 uint8_t servonum = 0;
-int maxServos = 2;
-uint16_t servoGoal[] = {SERVOMAX, SERVOMAX};
-uint16_t servoState[] = {SERVOMIN, SERVOMIN};
+int maxServos = 5;
+uint16_t servoGoal[] = {SERVOMIN, SERVOMIN, SERVOMIN, SERVOMIN, SERVOMIN};
+uint16_t servoState[] = {SERVOMIN, SERVOMIN, SERVOMIN, SERVOMIN, SERVOMIN};
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("8 channel Servo test!");
+  if (!Serial) delay(3000);
 
+  // ----------------------------------- Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  unsigned long stopWaiting = millis() + 5000;
+  while (WiFi.status() != WL_CONNECTED && (millis() < stopWaiting)) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  if(WiFi.status() == WL_CONNECTED) {
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("Starting UDP");
+    Udp.begin(localPort);
+    Serial.print("Local port: ");
+
+    Serial.println(localPort);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    useWifi = true;
+  } else {
+    Serial.println("WiFi not connected");
+  }
+
+  // ----------------------------------- PWM servo setup
   pwm.begin();
   /*
    * In theory the internal oscillator (clock) is 25MHz but it really isn't
@@ -72,25 +99,81 @@ void setup() {
 
 void loop() {
   // Let's start with 1 servos
-
+  //
   for (int i = 0; i < maxServos; i++) {
-    if (servoState[i] < servoGoal[i]) {
+    if (abs(servoState[i] - servoGoal[i]) > SERVO_SPEED) {
+      if (servoState[i] > servoGoal[i]) {
+        servoState[i] = servoState[i] - SERVO_SPEED;
+      } else {
+        servoState[i] = servoState[i] + SERVO_SPEED;
+      }
       pwm.setPWM(i, 0, servoState[i]);
-      servoState[i] = servoState[i]+10;
     }
   }
 
-  // if (servoState[0] < servoGoal[0]) {
-  //   pwm.setPWM(0, 0, servoState[0]);
-  //   servoState[0] = servoState[0]+10;
-  //   Serial.println(servoState[0]);
-  // }
-  //
-  // if (servoState[1] < servoGoal[1]) {
-  //   pwm.setPWM(1, 0, servoState[1]);
-  //   servoState[1] = servoState[1]+10;
-  //   Serial.println(servoState[1]);
-  // }
+  if (useWifi) {
+    OSCMessage msg;
+    int size = Udp.parsePacket();
+
+    if (size > 0) {
+      while (size--) {
+        msg.fill(Udp.read());
+      }
+      if (!msg.hasError()) {
+        msg.dispatch("/servo0", servo0);
+        // TODO
+        // msg.dispatch("/servo1", servo1);
+        // msg.dispatch("/servo2", servo2);
+        // msg.dispatch("/servo3", servo3);
+        // msg.dispatch("/servo4", servo4);
+        msg.dispatch("/ping", ping);
+      } else {
+        error = msg.getError();
+        Serial.print("error: ");
+        Serial.println(error);
+      }
+    }
+  }
 
   delay(50);
+}
+
+void ping(OSCMessage &msg) {
+  Serial.println("Ping!");
+}
+
+void servo0(OSCMessage &msg) {
+  int pos = msg.getInt(0);
+  servoGoal[0] = pos;
+  // pwm.setPWM(0, 0, pos);
+  Serial.print("/servo1 goal: ");
+  Serial.println(pos);
+}
+
+void servo1(OSCMessage &msg) {
+  int pos = msg.getInt(0);
+  servoGoal[1] = pos;
+  Serial.print("/servo1 goal: ");
+  Serial.println(pos);
+}
+
+void servo2(OSCMessage &msg) {
+  int pos = msg.getInt(0);
+  servoGoal[2] = pos;
+  Serial.print("/servo1 goal: ");
+  Serial.println(pos);
+}
+
+void servo3(OSCMessage &msg) {
+  int pos = msg.getInt(0);
+  servoGoal[3] = pos;
+  Serial.print("/servo1 goal: ");
+  Serial.println(pos);
+}
+
+void servo4(OSCMessage &msg) {
+  int pos = msg.getInt(0);
+  servoGoal[4] = pos;
+  Serial.print("/servo1 goal: ");
+  Serial.println(pos);
 }
